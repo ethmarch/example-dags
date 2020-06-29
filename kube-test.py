f067fd33686827f68e4cc3524f9f99ac8b8311bd
Alex Morreale <alexmorreale@me.com>
2020-06-29 18:51:31 -0400

    try this example

--- kube-test.py
+++ kube-test.py
@@ -1,32 +1,50 @@
-import airflow
 from airflow import DAG
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow import configuration as conf
 
 default_args = {
-    'owner': 'devops',
+    'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': airflow.utils.dates.days_ago(1),
-    'retries': 0
+    'start_date': datetime(2020, 6, 29),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG(
-    'run_container',
-    schedule_interval='*/30 * * * *',
-    default_args=default_args
-)
+namespace = "airflow" #conf.get('kubernetes', 'NAMESPACE')
 
+# This will detect the default namespace locally and read the
+# environment namespace when deployed to Astronomer.
+if namespace =='default':
+    config_file = '/usr/local/airflow/include/.kube/config'
+    in_cluster=False
+else:
+    in_cluster=True
+    config_file=None
+
+dag = DAG('example_kubernetes_pod',
+          schedule_interval='@once',
+          default_args=default_args)
+
+# This is where we define our desired resources.
+compute_resources = \
+  {'request_cpu': '800m',
+  'request_memory': '3Gi',
+  'limit_cpu': '800m',
+  'limit_memory': '3Gi'}
 
 with dag:
     k = KubernetesPodOperator(
-        name="my-container",
-        image=hello-world:latest,
-        namespace="airflow",
-        env_vars={
-            "RULES": "Testing"
-        },
-        task_id="run_container",
-        get_logs=True,
-        in_cluster=False,
-        xcom_push=False
-    )
+        namespace=namespace,
+        image="hello-world",
+        labels={"foo": "bar"},
+        name="airflow-test-pod",
+        task_id="task-one",
+        in_cluster=in_cluster # if set to true, will look in the cluster, if false, looks for file
+        cluster_context='docker-for-desktop', # is ignored when in_cluster is set to True
+        config_file=config_file,
+        resources=compute_resources,
+        is_delete_operator_pod=True,
+        get_logs=True)
