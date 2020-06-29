53f06e3e6dbec6bfeba3e7ac027563094799af0b
Alex Morreale <alexmorreale@me.com>
2020-06-29 18:11:16 -0400

    Update kube-test.py

--- kube-test.py
+++ kube-test.py
@@ -1,3 +1,9 @@
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.contrib.kubernetes.secret import Secret
+from airflow.contrib.kubernetes.volume import Volume
+from airflow.contrib.kubernetes.volume_mount import VolumeMount
+from airflow.contrib.kubernetes.pod import Port
+
 k = KubernetesPodOperator(namespace='airflow',
                           image="ubuntu:16.04",
                           cmds=["bash", "-cx"],
