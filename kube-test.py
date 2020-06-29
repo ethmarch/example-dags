4091e4974eb2f23463cc6d4be7a7d6ba7013e470
Alex Morreale <alexmorreale@me.com>
2020-06-29 18:53:37 -0400

    Update kube-test.py

--- kube-test.py
+++ kube-test.py
@@ -15,15 +15,6 @@ default_args = {
 
 namespace = "airflow" #conf.get('kubernetes', 'NAMESPACE')
 
-# This will detect the default namespace locally and read the
-# environment namespace when deployed to Astronomer.
-if namespace =='default':
-    config_file = '/usr/local/airflow/include/.kube/config'
-    in_cluster=False
-else:
-    in_cluster=True
-    config_file=None
-
 dag = DAG('example_kubernetes_pod',
           schedule_interval='@once',
           default_args=default_args)
@@ -42,9 +33,7 @@ with dag:
         labels={"foo": "bar"},
         name="airflow-test-pod",
         task_id="task-one",
-        in_cluster=in_cluster # if set to true, will look in the cluster, if false, looks for file
-        cluster_context='docker-for-desktop', # is ignored when in_cluster is set to True
-        config_file=config_file,
+        in_cluster=True
         resources=compute_resources,
         is_delete_operator_pod=True,
         get_logs=True)
