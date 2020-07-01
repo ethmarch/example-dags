f7df20a55d89e2ce20ba39b7bb2fda6982bb4e8a
Ethan March <ethan.march@ezcater.com>
2020-06-30 23:10:24 -0400

    undo other changes, fix import

--- kube-test.py
+++ kube-test.py
@@ -1,11 +1,10 @@
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
+from airflow.operators.bash import BashOperator
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from datetime import datetime
 from airflow.utils.dates import days_ago
 
 default_args = {
-    "owner": "airflow",
     "start_date": days_ago(2),
     "email_on_failure": False,
     "email_on_retry": False,
@@ -14,8 +13,8 @@ default_args = {
 
 dag = DAG("foobar", default_args=default_args, schedule_interval=None, catchup=False)
 
-t1 = BashOperator(task_id="foo", bash_command="echo 'foo'", dag=dag)
-t2 = BashOperator(task_id="bar", bash_command="echo 'bar'", dag=dag)
+t1 = BashOperator(task_id="foo", bash_command="echo foo", xcom_push=True, dag=dag)
+t2 = BashOperator(task_id="bar", bash_command="echo bar", dag=dag)
 
 t3 = KubernetesPodOperator(
     namespace="default",
